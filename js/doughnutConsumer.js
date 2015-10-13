102f59be772351f891cbb53fe721d5a771a62994
James White <cyberyt@gmail.com>
2015-10-13 18:30:47 +0100

    can add donuts and add em to page innit bruv

--- js/doughnutConsumer.js
+++ js/doughnutConsumer.js
@@ -17,15 +17,15 @@ function DoughnutController($http){
   }
   getDoughnuts()
 
-  self.addDoughnut;
+  self.addDoughnut = addDoughnut;
   self.newDoughnut = {};
 
   function addDoughnut() {
     $http
-      .post('http://api.doughnuts.ga/doughnuts')
-      .then(function(response, err) {
-        if (err) console.log(err);
+      .post('http://api.doughnuts.ga/doughnuts', self.newDoughnut)
+      .then(function(response) {
         console.log(response);
+        self.all.push(response.data)
       })
     self.newDoughnut = {};
   }
